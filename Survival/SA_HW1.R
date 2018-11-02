4c7f494d138c0f67f75de9532a3d6a5575d0c6dc
jlmichel <42470126+jlmichel@users.noreply.github.com>
2018-11-02 12:52:20 -0400

    Update Survival/SA_HW1.R

--- Survival/SA_HW1.R
+++ Survival/SA_HW1.R
@@ -24,6 +24,19 @@ sum(katrina$reason == 4) # 116/770
 sum(316,115,112,111,116) # matches 770
 
 summary(katrina)
+
+katrina$fail = katrina$survive == 0
+View(katrina)
+
+#if (katrina$survive == 0){
+#  katrina$fail = 1
+#} else {
+#  katrina$fail = 0
+#}
+#View(katrina)
+
+katrina$fail = 1 - katrina$survive
+  
 # Min = 1
 # 1st Quartile = 27
 # Median = 45
@@ -33,6 +46,27 @@ summary(katrina)
 
 ###### Survival Curves #######
 
-katrina_fit <- 
+katrina_fit = survfit(Surv(katrina$hour, katrina$fail == 1) ~ 1, data=katrina)
+katrina_fit
+summary(katrina_fit)
+plot(katrina_fit)
 
+# Stratify
+katrina.nofail = filter(katrina, reason == 0)
+katrina.flood = filter(katrina, reason == 1)
+katrina.motor = filter(katrina, reason == 2)
+katrina.surge = filter(katrina, reason == 3)
+katrina.jam = filter(katrina, reason == 4)
 
+# Survival Analysis of Stratified
+#nofail_fit = survfit(Surv(katrina.nofail$hour, katrina.nofail$fail == 1) ~ 1, data=katrina.nofail) # NO NEED
+flood_fit = survfit(Surv(katrina.flood$hour, katrina.flood$fail == 1) ~ 1, data=katrina.flood)
+motor_fit = survfit(Surv(katrina.motor$hour, katrina.motor$fail == 1) ~ 1, data=katrina.motor)
+surge_fit = survfit(Surv(katrina.surge$hour, katrina.surge$fail == 1) ~ 1, data=katrina.surge)
+jam_fit = survfit(Surv(katrina.jam$hour, katrina.jam$fail == 1) ~ 1, data=katrina.jam)
+
+# Plot them!
+plot(flood_fit)
+plot(motor_fit)
+plot(surge_fit)
+plot(jam_fit)

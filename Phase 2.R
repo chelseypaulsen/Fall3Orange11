3f85eae09f9138138045375fcec4b11646ceaeb0
jlmichel <42470126+jlmichel@users.noreply.github.com>
2018-11-14 13:17:11 -0500

    Update Phase 2.R

--- Phase 2.R
+++ Phase 2.R
@@ -9,12 +9,13 @@
 # Needed Libraries for Analysis #
 library(graphics)
 library(ks)
+# install.packages('triangle')
 library(triangle)
 library("readxl")
 
 #reading the data frame. Notice that that the rows are now 1-48 instead of 3-51 in the xlsx file. 
 #df <- read_xlsx("C:\\Users\\chels\\Desktop\\MSA\\Fall 3\\Simulation and Risk Analysis\\HW1\\Analysis_Data.xlsx",2,col_names=TRUE, col_types = c("date", "numeric", "numeric", "numeric", "numeric", "numeric", "numeric"), range='A3:G51')
-df <- read_xlsx("C:\\Users\\Steven\\Documents\\MSA\\Analytics Foundations\\Simulation and Risk\\data\\Analysis_Data.xlsx",2,col_names=TRUE, col_types = c("date", "numeric", "numeric", "numeric", "numeric", "numeric", "numeric"), range='A3:G51')
+df <- read_xlsx("C:\\Users\\jlmic\\Documents\\Simulation and Risk\\Data\\Analysis_Data.xlsx",2,col_names=TRUE, col_types = c("date", "numeric", "numeric", "numeric", "numeric", "numeric", "numeric"), range='A3:G51')
 
 #find mean and standard deviation of the 48 observations
 data = c(df$"Arithmetic Return - Crude Oil"[32:47],df$"Arithmetic Return - Natural Gas"[32:47],df$"Arithmetic Return - Dry Well"[32:47] )
@@ -128,71 +129,145 @@ mtext("Median", at=median(P2019k)+600 , col="darkorange3")
 # Then ultimately change the simulation.size to the desired size (10k or whatever massive number).
 # TODO Incorporate wet well/ dry well logic
 
+
+#####################################
+##### OBJECTIVES ####################
+##### 1. Cost of a singe dry well ###
+##### 2. NPV of a single wet well ### NPV = Net Present Value
+#####################################
+
 simulation.size <- 1
+
+
 
 #--------------------------------------#
 ###### Year Zero & Flat Expenses #######
 #--------------------------------------#
 
 ###### Leased acres per well Costs #######
-# Leased acres per well
-price.p.acre <- 9601
-acre.costs <- rnorm(simulation.size, mean=600, sd=50)*price.p.sec
+price.p.acre <- 960
+acre.costs <- rnorm(simulation.size, mean=600, sd=50)*price.p.acre
 
-###### Seismic Costs #######
+###### Seismic Costs per well Costs ######
 price.p.sec <- 43000 
 seismic.costs <- rnorm(simulation.size, mean=3, sd=0.35)*price.p.sec
 
 ###### Completion Costs #######
-completion.costs <- rnorm(simulation.size, mean=390000, sd=50000)
-
-
+# completion.costs <- rnorm(simulation.size, mean=390000, sd=50000)
+well_type = 'wet' # checked for 'wet' and 'dry'
+# Determine well logic (wet or dry)
+if (well_type == 'wet') {
+  completion.costs = rnorm(simulation.size, mean=390000, sd=50000)
+} else {
+  completion.costs = 0
+}
 
 ###### Professional Overhead #######
-# salary and benefit cost is best represented by a triangular distribution, 
-overhead <- rtriangle(simulation.size, a=172000, b=279500, c=215000)
-# Constant across the lifetime of a well, but potentially different for different wells. 
-# These costs are incurred during Year 0 as well for drilling, but stop after Year 0 if the well is dry.
-# TODO incorporate this cost into operating expenses.
+# salary and benefit cost is best represented by a triangular distribution: 
+# Need to incorporate how to determine it being different for different wells
+num.years = 0
+if (num.years == 0) {
+  prof.overhead <- rtriangle(simulation.size, a=172000, b=279500, c=215000)*(1)
+} else {
+  prof.overhead <- rtriangle(simulation.size, a=172000, b=279500, c=215000)*(1+num.years)
+}
 
-init.costs <- completion.costs+seismic.costs+acre.costs+overhead
+# Sum for total "Year 0 Expenses" = init.costs
+init.costs <- acre.costs + seismic.costs + completion.costs + prof.overhead
+init.costs
+
+
 
 #--------------------------------------#
 ############# Production Risk ##########
 #--------------------------------------#
 
-# IP= Initial production rate at time zero, with Lognormal dist, HINT in HW doc
-IP <- rlnorm(simulation.size, meanlog = 6, sdlog = 0.28)
+####### IP= Initial production rate at time zero, with Lognormal dist, HINT in HW doc
+simulation.size2 = 10000
+IP <- rlnorm(simulation.size2, meanlog = 6, sdlog = .28)
 
-# rate of decline is Uniformly distributed b/w 15 and 32 percent
-decline.rate = runif(n=number, min=0.15, max=0.32)
-rate=(1-decline.rate)*rate.prior
-vol = 365*(rate.prior+rate)/2
-# TODO imposed a correlation coefficient of 0.64 between the IP and the decline rate
-# TODO Build array for next 15(?) years of production rates
 
+####### Rate of decline is Uniformly distributed b/w 15 and 32 percent
+# YE = Year End | YB = Year Beginning
+decline.rate = runif(simulation.size2, min=0.15, max=0.32)
+# rate_YE=(1-decline.rate)*rate_YB  ### NOT SURE HOW TO APPLY THESE EQUATIONS
+# oil_vol = 365*(rate_YB+rate_YE)/2
+
+###### Impose Correlation ######
+R <- matrix(data=cbind(1, 0.64, 0.64, 1), nrow=2)
+U <- t(chol(R)) #choleski function
+
+# IP standarize # Mean = 420, sd = 120
+standardize_IP <- function(x){
+  ip.std = (x - 420)/120
+  return(ip.std)
+}
+
+destandardize_IP <- function(ip.std, x){
+  ip.old = (ip.std * 120) + 420
+  return(ip.old)
+}
+
+# decline.rate standardize # Mean unif = (a+b)/2 | Variance = ((b-a)^2)/12
+standardize_DR <- function(x){
+  dr.std = (x - 23.5)/4.907
+  return(dr.std)
+}
+
+destandardize_DR <- function(dr.std, x){
+  dr.old = (dr.std * 4.907) + 23.5
+  return(dr.old)
+}
+
+# Actually correlate
+Both.r <- cbind(standardize_IP(IP), standardize_DR(decline.rate))
+SB.r <- U %*% t(Both.r) # they become correlated multiplies in correlation structure # U is cholseki
+SB.r <- t(SB.r) # t() = transpose # put it back in the normal form
+
+final.SB.r <- cbind(destandardize_IP(SB.r[,1], S.r), destandardize_DR(SB.r[,2], B.r))
+final.SB.r
+
+# Not sure how to manage this for loop for the equations
+for (i in 1:15){
+  if (i == 1) {
+    rate_YE=(1-decline.rate)*IP  ### NOT SURE HOW TO APPLY THESE EQUATIONS
+    oil_vol = 365*(IP+rate_YE)/2
+  } else { 
+    rate_YE=(1-decline.rate)*rate_YB  ### NOT SURE HOW TO APPLY THESE EQUATIONS
+    oil_vol = 365*(rate_YB+rate_YE)/2
+  }
+}
+# Make for loop for calculating the equations
 #--------------------------------------#
 ############# Revenue Risk ##########
 #--------------------------------------#
+df2 = read_xlsx("C:\\Users\\jlmic\\Documents\\Simulation and Risk\\Data\\Analysis_Data.xlsx",1,col_names=TRUE,range='A3:D35')
+df2 = df2[1:15,]
 
 ######## Price of a barrel of crude oil ########
-# TODO Map Analysis_Data.xlsx data to lo, hi, expected variables below for 15yrs (2019-2033)
-oil.price <- rtriangle(simulation.size, a=lo, b=hi, c=expected)
-ann.rev = oil.price*ann.prod
+# Make an empty dataframe for the 15 oil prices
+oil = rep(0,15)
+for (i in 1:nrow(df2) ) {
+  oil.price <- rtriangle(simulation.size2, a=df2$`Low Oil Price`, b=df2$`High Oil Price`, c=df2$`AEO2018 Reference`)
+  oil[i] = oil.price
+}
+View(oil) # THis makes sense now
+hist(oil, breaks=50, col = 'cornflowerblue')#, main='2019 Cost Distribution Using Normal Approximation for 2006-2012', xlab='2019 Cost (Thousand Dollars)')
 
+
+
+  
 ######## Net Revenue Interest ########
 # annual revenues before the royalty payments are simply (Oil Price X Annual Production)
 # This calculation is done per well for the entire life of the well.
 nri = rnorm(simulation.size, mean=0.75, sd=0.02)
+ann.rev = oil.price*ann.prod
 ann.rev.diluted = ann.rev*nri
 
 
 #--------------------------------------#
 ############# Operating Expenses ##########
 #--------------------------------------#
-
-# TODO Build for operating expenses for every year
-# TODO ?Incoporate logic for a dry well?
 
 # Operating costs per barrel 
 # The expenses would be the same for every well in a given year,

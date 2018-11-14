9b0662cec7bee010acb49f4e72061abe9c1dfbe0
spowell3 <42471065+spowell3@users.noreply.github.com>
2018-11-13 20:11:47 -0500

    making distance calcs into a function

--- cluster/airbnb.py
+++ cluster/airbnb.py
@@ -55,8 +55,10 @@ def initial_cleaning(cal, listings, revs):
     return cal, listings
 
 def cal_extract(row):
-    """ EXTRACT CALENDAR INFO
+    """
+    EXTRACT CALENDAR INFO
     Call with `listing = listing.apply(cal_extract, axis=1)`
+
     :param row: single row of cleaned listing df (as series?)
             Also relies on global use of calendar
     :return: single row of listing df, with new columns of data summarized from calendar
@@ -117,7 +119,6 @@ def latlong_test():
     # Checked the answer in excel and it is right
 
 
-
 def euc_dist_attraction(name, attract_number):
     # Attempts at calculating euclidean distance of each listing to each attraction
     a = np.array((attractions.iloc[attract_number]['Latitude'], attractions.iloc[attract_number]['Longitude']))
@@ -130,114 +131,117 @@ def euc_dist_attraction(name, attract_number):
 # We will use the previous function to create a list of distances between each attraction and each listing separately
 
 ###### Calcs #######
-Fenway_dist = []
-euc_dist_attraction(Fenway_dist, 0)
-listings['Fenway_dist'] = pd.Series(Fenway_dist, index=listings.index)
+def dist_calcs(df):
+    Fenway_dist = []
+    euc_dist_attraction(Fenway_dist, 0)
+    df['Fenway_dist'] = pd.Series(Fenway_dist, index=df.index)
 
-Freedom_Trail_dist = []
-euc_dist_attraction(Freedom_Trail_dist, 1)
-listings['Freedom_Trail_dist'] = pd.Series(Freedom_Trail_dist, index=listings.index)
+    Freedom_Trail_dist = []
+    euc_dist_attraction(Freedom_Trail_dist, 1)
+    df['Freedom_Trail_dist'] = pd.Series(Freedom_Trail_dist, index=df.index)
 
-Fine_arts_dist = []
-euc_dist_attraction(Fine_arts_dist, 2)
-listings['Fine_arts_dist'] = pd.Series(Fine_arts_dist, index=listings.index)
+    Fine_arts_dist = []
+    euc_dist_attraction(Fine_arts_dist, 2)
+    df['Fine_arts_dist'] = pd.Series(Fine_arts_dist, index=df.index)
 
-Public_Library_dist = []
-euc_dist_attraction(Public_Library_dist, 3)
-listings['Public_Library_dist'] = pd.Series(Public_Library_dist, index=listings.index)
+    Public_Library_dist = []
+    euc_dist_attraction(Public_Library_dist, 3)
+    df['Public_Library_dist'] = pd.Series(Public_Library_dist, index=df.index)
 
-Public_Garden_dist = []
-euc_dist_attraction(Public_Garden_dist, 4)
-listings['Public_Garden_dist'] = pd.Series(Public_Garden_dist, index=listings.index)
+    Public_Garden_dist = []
+    euc_dist_attraction(Public_Garden_dist, 4)
+    df['Public_Garden_dist'] = pd.Series(Public_Garden_dist, index=df.index)
 
-Kennedy_Museum_dist = []
-euc_dist_attraction(Kennedy_Museum_dist, 5)
-listings['Kennedy_Museum_dist'] = pd.Series(Kennedy_Museum_dist, index=listings.index)
+    Kennedy_Museum_dist = []
+    euc_dist_attraction(Kennedy_Museum_dist, 5)
+    df['Kennedy_Museum_dist'] = pd.Series(Kennedy_Museum_dist, index=df.index)
 
-North_end_dist = []
-euc_dist_attraction(North_end_dist, 6)
-listings['North_end_dist'] = pd.Series(North_end_dist, index=listings.index)
+    North_end_dist = []
+    euc_dist_attraction(North_end_dist, 6)
+    df['North_end_dist'] = pd.Series(North_end_dist, index=df.index)
 
-Boston_pops_dist = []
-euc_dist_attraction(Boston_pops_dist, 7)
-listings['Boston_pops_dist'] = pd.Series(Boston_pops_dist, index=listings.index)
+    Boston_pops_dist = []
+    euc_dist_attraction(Boston_pops_dist, 7)
+    df['Boston_pops_dist'] = pd.Series(Boston_pops_dist, index=df.index)
 
-Sam_Adams_dist = []
-euc_dist_attraction(Sam_Adams_dist, 8)
-listings['Sam_Adams_dist'] = pd.Series(Sam_Adams_dist, index=listings.index)
+    Sam_Adams_dist = []
+    euc_dist_attraction(Sam_Adams_dist, 8)
+    df['Sam_Adams_dist'] = pd.Series(Sam_Adams_dist, index=df.index)
 
-Holocaust_Memorial_dist = []
-euc_dist_attraction(Holocaust_Memorial_dist, 9)
-listings['Holocaust_Memorial_dist'] = pd.Series(Holocaust_Memorial_dist, index=listings.index)
+    Holocaust_Memorial_dist = []
+    euc_dist_attraction(Holocaust_Memorial_dist, 9)
+    df['Holocaust_Memorial_dist'] = pd.Series(Holocaust_Memorial_dist, index=df.index)
 
-Boston_Harbor_dist = []
-euc_dist_attraction(Boston_Harbor_dist, 10)
-listings['Boston_Harbor_dist'] = pd.Series(Boston_Harbor_dist, index=listings.index)
+    Boston_Harbor_dist = []
+    euc_dist_attraction(Boston_Harbor_dist, 10)
+    df['Boston_Harbor_dist'] = pd.Series(Boston_Harbor_dist, index=df.index)
 
-Printing_office_dist = []
-euc_dist_attraction(Printing_office_dist, 11)
-listings['Printing_office_dist'] = pd.Series(Printing_office_dist, index=listings.index)
+    Printing_office_dist = []
+    euc_dist_attraction(Printing_office_dist, 11)
+    df['Printing_office_dist'] = pd.Series(Printing_office_dist, index=df.index)
 
-Greenway_dist = []
-euc_dist_attraction(Greenway_dist, 12)
-listings['Greenway_dist'] = pd.Series(Greenway_dist, index=listings.index)
+    Greenway_dist = []
+    euc_dist_attraction(Greenway_dist, 12)
+    df['Greenway_dist'] = pd.Series(Greenway_dist, index=df.index)
 
-Opera_House_dist = []
-euc_dist_attraction(Opera_House_dist, 13)
-listings['Opera_House_dist'] = pd.Series(Opera_House_dist, index=listings.index)
+    Opera_House_dist = []
+    euc_dist_attraction(Opera_House_dist, 13)
+    df['Opera_House_dist'] = pd.Series(Opera_House_dist, index=df.index)
 
-Back_bay_dist = []
-euc_dist_attraction(Back_bay_dist, 14)
-listings['Back_bay_dist'] = pd.Series(Back_bay_dist, index=listings.index)
+    Back_bay_dist = []
+    euc_dist_attraction(Back_bay_dist, 14)
+    df['Back_bay_dist'] = pd.Series(Back_bay_dist, index=df.index)
 
-Arnold_Arboretum_dist = []
-euc_dist_attraction(Arnold_Arboretum_dist, 15)
-listings['Arnold_Arboretum_dist'] = pd.Series(Arnold_Arboretum_dist, index=listings.index)
+    Arnold_Arboretum_dist = []
+    euc_dist_attraction(Arnold_Arboretum_dist, 15)
+    df['Arnold_Arboretum_dist'] = pd.Series(Arnold_Arboretum_dist, index=df.index)
 
-Bunker_hill_dist = []
-euc_dist_attraction(Bunker_hill_dist, 16)
-listings['Bunker_hill_dist'] = pd.Series(Bunker_hill_dist, index=listings.index)
+    Bunker_hill_dist = []
+    euc_dist_attraction(Bunker_hill_dist, 16)
+    df['Bunker_hill_dist'] = pd.Series(Bunker_hill_dist, index=df.index)
 
-Isabella_Stewart_Museum_dist = []
-euc_dist_attraction(Isabella_Stewart_Museum_dist, 17)
-listings['Isabella_Stewart_Museum_dist'] = pd.Series(Isabella_Stewart_Museum_dist, index=listings.index)
+    Isabella_Stewart_Museum_dist = []
+    euc_dist_attraction(Isabella_Stewart_Museum_dist, 17)
+    df['Isabella_Stewart_Museum_dist'] = pd.Series(Isabella_Stewart_Museum_dist, index=df.index)
 
-Kennedy_Institute_dist = []
-euc_dist_attraction(Kennedy_Institute_dist, 18)
-listings['Kennedy_Institute_dist'] = pd.Series(Kennedy_Institute_dist, index=listings.index)
+    Kennedy_Institute_dist = []
+    euc_dist_attraction(Kennedy_Institute_dist, 18)
+    df['Kennedy_Institute_dist'] = pd.Series(Kennedy_Institute_dist, index=df.index)
 
-Tea_Party_dist = []
-euc_dist_attraction(Tea_Party_dist, 19)
-listings['Tea_Party_dist'] = pd.Series(Tea_Party_dist, index=listings.index)
+    Tea_Party_dist = []
+    euc_dist_attraction(Tea_Party_dist, 19)
+    df['Tea_Party_dist'] = pd.Series(Tea_Party_dist, index=df.index)
 
-Beacon_hill_dist = []
-euc_dist_attraction(Beacon_hill_dist, 20)
-listings['Beacon_hill_dist'] = pd.Series(Beacon_hill_dist, index=listings.index)
+    Beacon_hill_dist = []
+    euc_dist_attraction(Beacon_hill_dist, 20)
+    df['Beacon_hill_dist'] = pd.Series(Beacon_hill_dist, index=df.index)
 
-Old_North_Church_dist = []
-euc_dist_attraction(Old_North_Church_dist, 21)
-listings['Old_North_Church_dist'] = pd.Series(Old_North_Church_dist, index=listings.index)
+    Old_North_Church_dist = []
+    euc_dist_attraction(Old_North_Church_dist, 21)
+    df['Old_North_Church_dist'] = pd.Series(Old_North_Church_dist, index=df.index)
 
-Faneuil_Hall_dist = []
-euc_dist_attraction(Faneuil_Hall_dist, 22)
-listings['Faneuil_Hall_dist'] = pd.Series(Faneuil_Hall_dist, index=listings.index)
+    Faneuil_Hall_dist = []
+    euc_dist_attraction(Faneuil_Hall_dist, 22)
+    df['Faneuil_Hall_dist'] = pd.Series(Faneuil_Hall_dist, index=df.index)
 
-Aquarium_dist = []
-euc_dist_attraction(Aquarium_dist, 23)
-listings['Aquarium_dist'] = pd.Series(Aquarium_dist, index=listings.index)
+    Aquarium_dist = []
+    euc_dist_attraction(Aquarium_dist, 23)
+    df['Aquarium_dist'] = pd.Series(Aquarium_dist, index=df.index)
 
-Museum_of_Science_dist = []
-euc_dist_attraction(Museum_of_Science_dist, 24)
-listings['Museum_of_Science_dist'] = pd.Series(Museum_of_Science_dist, index=listings.index)
+    Museum_of_Science_dist = []
+    euc_dist_attraction(Museum_of_Science_dist, 24)
+    df['Museum_of_Science_dist'] = pd.Series(Museum_of_Science_dist, index=df.index)
 
-TD_Garden_dist = []
-euc_dist_attraction(TD_Garden_dist, 25)
-listings['TD_Garden_dist'] = pd.Series(TD_Garden_dist, index=listings.index)
+    TD_Garden_dist = []
+    euc_dist_attraction(TD_Garden_dist, 25)
+    df['TD_Garden_dist'] = pd.Series(TD_Garden_dist, index=df.index)
 
+    print("Calculated distances of listings to attractions. \n")
+    return df
 
-print("Calculated distances of listings to attractions. \n")
+listings_dist = dist_calcs(listings)
 
-pkl.dump(listings, open(os.path.join('pickles', 'listings_dist'), "wb"))
+pkl.dump(listings_dist, open(os.path.join('pickles', 'listings_dist'), "wb"))
 # listings = pkl.load(open(os.path.join('pickles', 'listings_dist'), "rb"))
 
 
@@ -268,11 +272,11 @@ def sentiment_extr(revs):
     # Plot this sentiment distribution
     sns.distplot(reviews_informative['compound'])  # looks GREAT! well... at least not weird
 
+    print("Sentiments calculated for each review. \n")
     return reviews_informative
 
 
 reviews = sentiment_extr(reviews)
-print("Sentiments calculated for each review. \n")
 
 pkl.dump(reviews, open(os.path.join('pickles', 'reviews_sent'), "wb"))
 # reviews = pkl.load(open(os.path.join('pickles', 'reviews_sent'), "rb"))
@@ -297,7 +301,7 @@ list_clust = listing10 # selection of prepared df for clustering
 from sklearn.cluster import KMeans
 
 kmeans = KMeans(n_clusters=5, random_state=8)
-sent_temp = list_clust[['compound mean', 'compound std']]  # TODO Should we normalize these???
+sent_temp = list_clust[['compound mean', 'compound std']]  # TODO Should we normalize these?
 # list_temp = listing_clust[['','','',]] # TODO Should use other metrics for clustering too
 
 kmeans.fit(sent_temp.dropna())

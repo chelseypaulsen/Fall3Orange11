90fbf8226326f4bde4ea1258d1484b0439d8f645
jlmichel <42470126+jlmichel@users.noreply.github.com>
2018-11-25 21:42:05 -0500

    Update ML Project RF.R

--- ML Project RF.R
+++ ML Project RF.R
@@ -12,9 +12,6 @@ df_ML = read.csv('C:\\Users\\jlmic\\Documents\\Machine Learning\\Data\\MLProject
 #df_ML = read.csv('C:\\Users\\Steven\\Documents\\MSA\\Analytics Foundations\\Machine Learning\\data\\MLProjectData.csv')
 #df_ML= read.csv('C:\\Users\\chels\\Desktop\\MSA\\Fall 3\\Machine Learning\\Project\\MLProjectData.csv')
 
-# Be careful to not include this ID column in analysis
-df_ML$ID <- seq(1,dim(df_ML)[1])
-
 # column groups
 cat.col = c('cat1','cat2','cat3','cat4','cat5','cat6','cat7','cat8','cat9','cat10','cat11','cat12','cat13','cat14','cat15',
             'cat16','cat17','cat18','cat19','cat20','cat21','cat22','cat23','cat24','cat25','cat26')
@@ -29,7 +26,6 @@ df_ML_validate<-df_ML[-intrain,]
 
 dim(df_ML_train)
 dim(df_ML_validate)
-View(df_ML_train)
 
 ############################################################################
 #Exploration#
@@ -39,105 +35,105 @@ library(corrplot)
 summary(df_ML) # no missing values to worry about
 
 # Basic plots for exploring if we want them
-ggplot(df_ML_train) +
-  geom_histogram(aes(x=target, fill= cat13))
-
-ggplot(df_ML_train) +
-  geom_point(aes(x=num29, y=target, color= cat1), alpha=0.1)
-
-ggplot(df_ML_train) +
-  geom_histogram(aes(x=target)) +
-  facet_wrap(~ cat1) 
+# ggplot(df_ML_train) +
+#   geom_histogram(aes(x=target, fill= cat13))
+# 
+# ggplot(df_ML_train) +
+#   geom_point(aes(x=num29, y=target, color= cat1), alpha=0.1)
+# 
+# ggplot(df_ML_train) +
+#   geom_histogram(aes(x=target)) +
+#   facet_wrap(~ cat1) 
 
 
 #Correlation matrix
-corrplot(cor(df_ML[c(num.col,"target")]))
+#corrplot(cor(df_ML[c(num.col,"target")]))
 # num2 and num8-11 are strongly correlated
 # num35-42 are strongly correlated with one another
 # num44-54 are strongly correlated with one another
 # num35-54 have stron negative correlations with num44=54
 # essentially no correlation b/w any numeric variable and the target... WTF? Is the target just random numbers?
-corrplot.mixed(cor(df_ML[c(num.col[10:20],"target")])) # weird negative correlation b/w num15-17 and num18-20
+#corrplot.mixed(cor(df_ML[c(num.col[10:20],"target")])) # weird negative correlation b/w num15-17 and num18-20
 
-# Frequency tables
-attach(df_ML)
-MLtable <- table(cat1, cat2)
-prop.table(MLtable)
-rm(MLtable)
-# almost even distribution across categorical columns
-
-# "ID", "target"
-# Histograms by variable, definitely some numeric columns with the same distributions
-df_ML %>%
-  keep(is.numeric) %>% 
-  gather() %>% 
-  ggplot(data=., aes(value)) +
-    facet_wrap(~ key, scales = "free") +
-    geom_histogram()
-
-df_ML %>%
-  select(which(sapply(.,class)=="numeric"),target,ID) %>% 
-  gather() %>%
-  ggplot(data=id, aes(x=ID, y=value)) +
-    facet_wrap(~ key, scales = "free") +
-    geom_point()
+# # Frequency tables
+# attach(df_ML)
+# MLtable <- table(cat1, cat2)
+# prop.table(MLtable)
+# rm(MLtable)
+# # almost even distribution across categorical columns
+# 
+# # "ID", "target"
+# # Histograms by variable, definitely some numeric columns with the same distributions
+# df_ML %>%
+#   keep(is.numeric) %>% 
+#   gather() %>% 
+#   ggplot(data=., aes(value)) +
+#     facet_wrap(~ key, scales = "free") +
+#     geom_histogram()
+# 
+# df_ML %>%
+#   select(which(sapply(.,class)=="numeric"),target,ID) %>% 
+#   gather() %>%
+#   ggplot(data=id, aes(x=ID, y=value)) +
+#     facet_wrap(~ key, scales = "free") +
+#     geom_point()
 
 
 ############################################################################
 #Random Forest#
 ############################################################################
 
-# Assign Random Forest Model
+##################################
+# Build Random Forest #
+##################################
 library(randomForest)
-rf_ML = randomForest(target ~ ., data=df_ML_train, importance=TRUE, ntree=50)
-print(rf_ML)
+library(ModelMetrics)
 
+#### find best mtry/ntree#### WARNING: THIS WILL TAKE HOURS TO RUN
+# accuracy=vector()
+# mtry=seq(2,86,by=2)
+# ntree=seq(25,200,by=25)
+# i=1
+# for (n in ntree) {
+#   for(m in mtry){
+#     print(paste(n,m))
+#     rf = randomForest(target ~ ., data=df_ML_train,mtry=m, ntree=n)
+#     test_pred =  predict(rf,df_ML_validate)
+#     #accuracy[i] =  sum(test_pred!=df_ML_train$target)/nrow(df_ML_train)
+#     abserror = abs(df_ML_validate$target - test_pred)
+#     accuracy[i] = mean(abserror)
+#     i=i+1
+#   }
+# }
 
-# Check Variable Importance
-importance(rf_ML) # all important
+# CHOSEN MODEL mtry=24, ntree=175 - MAE = 0.39/0.95
+rf_ML = randomForest(target ~ ., data=df_ML_train, importance=TRUE, mtry=24, ntree=175)
 
-## Predict on training data and calculate MAE##
+# RF on Training Data #
 rf_train_pred = predict(rf_ML, df_ML_train)
-
-View(rf_train_pred)
 
 df_ML_train$Prediction = rf_train_pred
 df_ML_train$abserror = abs(df_ML_train$target - df_ML_train$Prediction)
-dim(df_ML_train)
 
 MAE = mean(df_ML_train$abserror)
 print(MAE)
 
-##################################
-# Predict on Validate Data #
-##################################
-
-dim(df_ML_validate)
-
+# RF on Validate Data #
 rf_valid_pred = predict(rf_ML, df_ML_validate)
-
-View(rf_valid_pred)
 
 df_ML_validate$Prediction = rf_valid_pred
 df_ML_validate$abserror = abs(df_ML_validate$target - df_ML_validate$Prediction)
-dim(df_ML_validate)
 
 MAE = mean(df_ML_validate$abserror)
 print(MAE)
 
-##################################
-# Predict on test Data for Submission#
-##################################
-
-df_ML_test = select(df_ML_test, -X)
-View(df_ML_test)
-
-
-
+############################################################################
 ############################################################################
 #XGBoost#
 ############################################################################
+############################################################################
 library('Matrix')
+#install.packages('xgboost')
 library(xgboost)
 library(readr)
 library(stringr)
@@ -146,45 +142,249 @@ library(car)
 
 ############# Copied from above ###############################################################################
 
-df_XG <- df_ML
-df_XG_train <- df_ML_train
-df_XG_validate <- df_ML_validate
-#View(df_XG)
+df_XG_train<-df_ML[intrain,]
+df_XG_validate<-df_ML[-intrain,]
 
 ##############################################################################################################
 
-#All vars need to be numeric (or factors)
-
 #Changing all categorical vars to factors
-f_XG_train[cat.col] = lapply(df_XG_train[cat.col], factor)
+df_XG_train[cat.col] = lapply(df_XG_train[cat.col], factor)
 df_XG_validate[cat.col] = lapply(df_XG_validate[cat.col], factor)
-#Checking if it worked
-str(df_XG_train)
-str(df_XG_validate)
-
 
 sparse_train = sparse.model.matrix(target ~ . -target , data=df_XG_train)
 sparse_valid = sparse.model.matrix(target  ~ . -target , data=df_XG_validate)
 train_label = as.numeric(df_XG_train$target)[df_XG_train$target] 
 
+# # Tune for eta
+# accuracy_xgb=vector()
+# eta=seq(.05,1,by=.05)
+# i=1
+# for (e in eta) {
+#   print(i)
+#   xgb <- xgboost(data = sparse_train,
+#                      label = train_label,
+#                      eta = 0,
+#                      max_depth = 20,
+#                      gamma = 0,
+#                      nround=100,
+#                      subsample = 0.75,
+#                      colsample_bytree = 0.75,
+#                      objective = "reg:linear",
+#                      nthread = 3,
+#                      eval_metric = 'rmse',
+#                      verbose =0)
+#   ptrain = predict(xgb, sparse_train)
+#   pvalid = predict(xgb, sparse_valid)
+#   abserror = abs(df_XG_validate$target - pvalid)
+#   accuracy_xgb[i] = mean(abserror)
+#   i=i+1
+# }
+# plot(eta, accuracy_xgb)
 
-# tune and run the model
+# Final XGB Model - MAE = 1.01, 0.94
 xgb <- xgboost(data = sparse_train,
-                   label = train_label,
-                   eta = 0.05,
-                   max_depth = 15,
-                   gamma = 0,
-                   nround=100,
-                   subsample = 0.75,
-                   colsample_bytree = 0.75,
-                   objective = "reg:linear",
-                   nthread = 3,
-                   eval_metric = 'rmse',
-                   verbose =0)
+               label = train_label,
+               eta = 0.05,
+               max_depth = 5,
+               gamma = 0.1,
+               nround=100,
+               subsample = 0.75,
+               colsample_bytree = 0.75,
+               objective = "reg:linear",
+               nthread = 3,
+               eval_metric = 'rmse',
+               verbose =0)
+
+# PRedict
 ptrain = predict(xgb, sparse_train)
 pvalid = predict(xgb, sparse_valid)
-cor(ptrain,df_XG_train$target) ###not really sure what the correlation tells us
 
-print(xgb)
+# MAE
+df_XG_train$Prediction = ptrain
+df_XG_train$abserror = abs(df_XG_train$target - df_XG_train$Prediction)
 
-########## NEED TO DO MODEL ASSESSMENT AND PREDICTIONS HOW SHE SPECIFIED ########### 
+MAE = mean(df_XG_train$abserror)
+print(MAE)
+
+df_XG_validate$Prediction = pvalid
+df_XG_validate$abserror = abs(df_XG_validate$target - df_XG_validate$Prediction)
+
+MAE = mean(df_XG_validate$abserror)
+print(MAE)
+
+
+
+############################################################################
+############################################################################
+# Support Vector Machine (SVM) #
+############################################################################
+############################################################################
+
+df_SVM_train<-df_ML[intrain,]
+df_SVM_validate<-df_ML[-intrain,]
+
+###########################################################################
+############       STANDARDIZE THE CONTINUOUS VARIABLES        ############
+###########################################################################
+###########################################################################
+
+SVM_trainscale = scale(df_SVM_train[,1:59], center=T, scale=T)
+SVM_trainscale2 = cbind(SVM_trainscale, df_SVM_train[,60:86])
+
+SVM_validscale = scale(df_SVM_validate[,1:59], center=T, scale=T)
+SVM_validscale2 = cbind(SVM_validscale, df_SVM_validate[,60:86])
+
+#install.packages("e1071")
+library(e1071)
+###########################################################################
+###########################################################################
+############     TUNE THE SVM HYPERPARAMETERS GAMMA AND C - Takes an eternity     ############
+###########################################################################
+###########################################################################
+# TuneSVM = tune.svm(target~., data=SVM_trainscale, kernel='radial', gamma=2^(-5:-.5), cost=2^(-1:3))
+# summary(TuneSVM)
+# plot(TuneSVM)
+
+# - Detailed performance results:
+#   gamma cost error dispersion
+# 1  0.031  0.5   2.2       0.36
+# 2  0.062  0.5   2.2       0.36
+# 3  0.125  0.5   2.1       0.35
+# 4  0.250  0.5   2.1       0.35
+# 5  0.500  0.5   2.1       0.35
+# 6  0.031  1.0   2.2       0.37
+# 7  0.062  1.0   2.2       0.36
+# 8  0.125  1.0   2.1       0.35
+# 9  0.250  1.0   2.1       0.35
+# 10 0.500  1.0   2.1       0.35
+# 11 0.031  2.0   2.3       0.38
+# 12 0.062  2.0   2.3       0.37
+# 13 0.125  2.0   2.2       0.35
+# 14 0.250  2.0   2.1       0.35
+# 15 0.500  2.0   2.1       0.35
+# 16 0.031  4.0   2.4       0.39
+# 17 0.062  4.0   2.3       0.38
+# 18 0.125  4.0   2.2       0.36
+# 19 0.250  4.0   2.1       0.35
+# 20 0.500  4.0   2.1       0.36
+# 21 0.031  8.0   2.5       0.40
+# 22 0.062  8.0   2.3       0.38
+# 23 0.125  8.0   2.2       0.36
+# 24 0.250  8.0   2.1       0.35
+# 25 0.500  8.0   2.1       0.36
+
+############################################################################
+############################################################################
+## USE THE BEST HYPERPARAMETERS TO BUILD FINAL MODEL AND TEST PERFORMANCE ##
+############################################################################
+############################################################################
+
+#### Final SVM Model - MAE = 0.34/0.93 ####
+bestgamma=0.5
+bestc=1
+svm1=svm(target~., data=SVM_trainscale2, kernel='radial', gamma=bestgamma, cost=bestc)
+
+# Predict
+pred=predict(svm1,SVM_trainscale2)
+pred2=predict(svm1,SVM_validscale2)
+
+# MAE
+df_SVM_train$Prediction = pred
+df_SVM_train$abserror = abs(df_SVM_train$target - df_SVM_train$Prediction)
+
+MAE = mean(df_SVM_train$abserror)
+print(MAE)
+
+df_SVM_validate$Prediction = pred2
+df_SVM_validate$abserror = abs(df_SVM_validate$target - df_SVM_validate$Prediction)
+
+MAE = mean(df_SVM_validate$abserror)
+print(MAE)
+
+
+############################################################################
+## Neural Network ## - This is not working
+############################################################################
+#install.packages('neuralnet')
+library(neuralnet)
+
+#install.packages('fastDummies')
+library(fastDummies)
+
+# Make normalized neural network dataset
+df_NN = as.data.frame(scale(df_ML[,c(1:59,86)], center=T, scale=T)) # mean and sd scaling
+df_NN = cbind(df_NN, df_ML[,60:85])
+df_NN = dummy_cols(df_NN)
+df_NN = df_NN[,c(1:60,87:103)]
+
+set.seed(8) # The greatest number there ever was
+intrain<-createDataPartition(y=df_ML$target,p=0.7,list=FALSE)
+df_NN_train<-df_NN[intrain,]
+df_NN_validate<-df_NN[-intrain,]
+dim(df_NN_train)
+f = as.formula(target~num1+num2+num3+num4+num5+num6+num7+num8+num9+num10+num11+num12+num13+num14+num15+num16+num17+num18+num19+num20+
+                 num21+num22+num23+num24+num25+num26+num27+num28+num29+num30+num31+num32+num33+num34+num35+num36+num37+num38+num39+num40+
+                 num41+num42+num43+num44+num45+num46+num47+num48+num49+num50+num51+num52+num53+num54+num55+num56+num57+num58+num59+
+                 cat1_E+cat1_A+cat1_C+cat1_D+cat1_B+cat2_D+cat2_G+cat2_L+cat2_B+cat2_F+cat2_H+cat2_C+cat2_K+cat2_E+cat2_I+cat2_A+cat2_J)
+
+nnet1 = neuralnet(f, data=df_NN_train, hidden=1)
+nnet1
+results1 = compute(nnet1, df_NN_valid) # This line is breaking
+nnet1Pred=results1$net.result
+
+############################################################################
+############################################################################
+# What about just the mean of the target? MAE = 0.97/0.93
+############################################################################
+############################################################################
+mean = mean(df_ML$target)
+df_MEAN_train<-df_ML[intrain,]
+df_MEAN_validate<-df_ML[-intrain,]
+
+# MAE
+df_MEAN_train$Prediction = mean
+df_MEAN_train$abserror = abs(df_MEAN_train$target - df_MEAN_train$Prediction)
+
+MAE = mean(df_MEAN_train$abserror)
+print(MAE)
+
+df_MEAN_validate$Prediction = mean
+df_MEAN_validate$abserror = abs(df_MEAN_validate$target - df_MEAN_validate$Prediction)
+
+MAE = mean(df_MEAN_validate$abserror)
+print(MAE)
+
+############################################################################
+############################################################################
+# SVM Chosen Test Data
+############################################################################
+############################################################################
+
+# Predict on test
+testData = read.csv('C:\\Users\\jlmic\\Documents\\Machine Learning\\Data\\testData.csv')
+
+testData = testData[,2:86]
+testData_scale = scale(testData[,1:59], center=T, scale=T)
+testData_scale2 = cbind(testData_scale, testData[,60:85])
+
+unique(testData_scale2$cat26) # Checked all unique values of all categorical variables and there are none in testdata that aren't in validation
+unique(SVM_validscale2$cat26)
+
+View(testData_scale2)
+View(SVM_validscale2)
+dim(SVM_validscale2)
+dim(testData_scale2)
+
+bestgamma=0.5
+bestc=1
+svm1=svm(target~., data=SVM_trainscale2, kernel='radial', gamma=bestgamma, cost=bestc)
+
+# Predict
+test_pred=predict(svm1,testData_scale2) # NO clue why this is breaking. 
+
+
+# MAE
+df_SVM_train$Prediction = pred
+
+
+
